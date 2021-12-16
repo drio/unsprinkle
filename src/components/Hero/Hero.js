ce98151c482b3d08082dfdfe4c3355e1671dcdc9
David Rio Deiros <driodeiros@gmail.com>
2021-12-16 13:55:49 -0500

    step 2.5

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -1,10 +1,31 @@
-import React from 'react';
-import styled from 'styled-components/macro';
+import React from "react";
+import styled from "styled-components/macro";
 
 const Hero = () => {
   return (
     <Wrapper>
-      <HeroImage src="/images/hero-img.jpg" />
+      <picture>
+        <source
+          type="image/avif"
+          srcSet={`
+                            /images/hero-img.avif 1x,
+                            /images/hero-img@2x.avif 2x,
+                            /images/hero-img@3x.avif 3x
+                                      `}
+        />
+        <source
+          type="image/jpeg"
+          srcSet={`
+                    /images/hero-img.jpg 1x,
+                    /images/hero-img@2x.jpg 2x,
+                    /images/hero-img@3x.jpg 3x
+                  `}
+        />
+        <HeroImage
+          alt="An example of the photos distributed through this site (cat on black background)"
+          src="/images/hero-img.jpg"
+        />
+      </picture>
       <Swoop src="/swoop.svg" />
     </Wrapper>
   );
